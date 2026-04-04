d51f394bbe765af080ec74438ffc0ac55418203e
hushiyang <hushiyang@gmail.com>
2026-04-04 23:31:17 +0800

    chore: 更新配置以支持 GitHub Pages 部署

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/daosoon-website-v2" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
